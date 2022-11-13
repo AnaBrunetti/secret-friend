255eebf119fe9a87afd231c5a942ed1f67dde4bb
Ana Brunetti <carol-brunetti@hotmail.com>
2022-11-13 06:53:10 -0300

    Change styles on fixes pages

--- website/views.py
+++ website/views.py
@@ -2,12 +2,14 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext_lazy as _
-from website.forms import ContactForm
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
-from django.urls import reverse
+from django.urls import reverse_lazy
+
+from website.forms import ContactForm
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -16,12 +18,15 @@ class HomeView(TemplateView):
     def dispatch(self, *args, **kwargs):
         return super(HomeView, self).dispatch(*args, **kwargs)
     
+
 class AboutView(TemplateView):
     template_name = 'general/about.html'
     
+
 class TermsView(TemplateView):
     template_name = 'general/terms.html'
-    
+
+
 class ContactView(TemplateView, FormView, SuccessMessageMixin):
     template_name = 'general/contact.html'
     form_class = ContactForm
@@ -29,7 +34,7 @@ class ContactView(TemplateView, FormView, SuccessMessageMixin):
     
     def get_success_url(self):
         messages.add_message(self.request, messages.INFO, 'form submission success')
-        return reverse('contact:contact')
+        return reverse_lazy('contact')
     
     def post(self, request, *args, **kwargs):
         form = self.get_form()
@@ -38,4 +43,3 @@ class ContactView(TemplateView, FormView, SuccessMessageMixin):
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
-
